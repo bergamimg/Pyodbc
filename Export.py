2385c0fa89f900f7a6b47dea0caf67a35f1aae8f
bergamimg <53324245+bergamimg@users.noreply.github.com>
2020-10-26 15:33:14 -0300

    commit_one
    Python scripts using Pyodbc and also Pandas modules to:
    - Manage and format local files
    - Export and Import querys in order to generate database reports

---
+++ Export.py
@@ -0,0 +1,55 @@
+﻿import pyodbc 
+import pandas as pd
+import time
+import xlrd
+import openpyxl
+
+## Get time of execution 
+start_time = time.time()
+
+## Define path for result file
+excel_file = r'C:\Users\Desktop\GitHub\2.Pyodbc\Export.xlsx'
+
+## Connection Properties
+user = 'user_001'
+password = 'password_001'
+server = 'server_001'
+database = 'database_001'
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server='+server+';'
+                      'Database='+database+';'
+                      'Trusted_Connection=no;'
+                      'uid='+user+';'
+                      'pwd='+password+';'
+                      )
+pyodbc.pooling = False
+cursor = conn.cursor()
+cursor.fast_executemany = True
+
+query = ''' SELECT 
+              [UF], [NUM_ENVIO], CONVERT(INT,[QTDE_INFO]), 
+              [INVOICE_NUMBER], [VALOR], [MES], [REGISTRO_ID]
+              
+            FROM [DataBase].[dbo].[Tabela] 
+            WHERE [MES] in ('setembro_20')
+        '''
+cursor.execute(query)
+
+columns = [column[0] for column in cursor.description]
+
+df = pd.DataFrame(columns = columns)
+
+## Appending the cursor rows to a DataFrame
+for row in cursor:
+       line = {}
+       for index,value in enumerate(row):
+              line[columns[index]] = value
+       df = df.append(line, ignore_index=True)
+
+## Convert to float Money Value      
+df['VALOR'] = df['VALOR'].astype(float)
+
+## Exporting the DataFrame to an Excel Sheet
+df.to_excel (excel_file, index = None, header=True, engine="xlsxwriter", float_format="%.3f") 
+conn.close()
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ Import.py
@@ -0,0 +1,70 @@
+﻿import pyodbc 
+import pandas as pd
+import time
+import xlrd
+import openpyxl
+
+## python -m pip install library --user --no-warn-script-location --proxy http://proxy.company.enterprise:0000
+
+## Get time of execution 
+start_time = time.time()
+
+## Define path for importable file
+excel_file = r'C:\Users\Desktop\GitHub\2.Pyodbc\Export.xlsx'
+df = pd.read_excel(excel_file, header=0, na_filter=False, sheet_name='Sheet1')
+df2 = pd.DataFrame(df, columns = ['UF', 'NUM_ENVIO', 'QTDE_INFO', 'INVOICE_NUMBER', 'VALOR', 'MES', 'REGISTRO_ID'])
+
+## Connection Properties
+user = 'user_001'
+password = 'password_001'
+server = 'server_001'
+database = 'database_001'
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server='+server+';'
+                      'Database='+database+';'
+                      'Trusted_Connection=no;'
+                      'uid='+user+';'
+                      'pwd='+password+';'
+                      )
+pyodbc.pooling = False
+cursor = conn.cursor()
+cursor.fast_executemany = True
+
+query1 = ''' UPDATE [DataBase].[dbo].[Tabela] with (updlock, serializable)
+             SET  [UF] = ?,
+                  [NUM_ENVIO] = ?,
+                  [QTDE_INFO] = ?, 
+                  [INVOICE_NUMBER] = ?, 
+                  [VALOR] = ?, 
+                  [MES] = ?
+             WHERE [REGISTRO_ID] = ?  
+         '''     
+
+query2 = ''' INSERT INTO [DataBase].[dbo].[Tabela] with (updlock, serializable)
+            (  [UF],
+               [NUM_ENVIO],
+               [QTDE_INFO], 
+               [INVOICE_NUMBER], 
+               [VALOR], 
+               [MES]
+            )
+              values 
+              (?, ?, ?, ?, ?, ?) 
+         '''
+
+## Filter DataFrame, one with pre-existing values, one with new values
+df_existing_values = df2[(df2.REGISTRO_ID != '') | (df2.REGISTRO_ID != 'None') | (df2.REGISTRO_ID != 'NULL') ]
+df_new_values = df2[(df2.REGISTRO_ID == '') | (df2.REGISTRO_ID == 'None') | (df2.REGISTRO_ID == 'NULL') ]
+
+## Update pre-existing values
+for row in df_existing_values.itertuples():
+   cursor.execute(query1, row.UF, row.NUM_ENVIO, row.QTDE_INFO, row.INVOICE_NUMBER, row.VALOR, row.MES, row.REGISTRO_ID)
+   conn.commit()
+
+## Insert new values
+for row in df_new_values.itertuples():
+   cursor.execute(query2, row.UF, row.NUM_ENVIO, row.QTDE_INFO, row.INVOICE_NUMBER, row.VALOR, row.MES, row.REGISTRO_ID)
+   conn.commit()   
+
+conn.close()
+print("--- %s seconds ---" % (time.time() - start_time))
